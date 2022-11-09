7b524aedee8b83dceb52e7ddaaa809dc38233d58
Anuj Pundalik <anuj.pundalik@contentstack.com>
2022-11-09 12:05:37 +0530

    Remove unwanted files

---
+++ pages/api/cloud-function.js
@@ -0,0 +1,9 @@
+export default function handler(_, res) {
+  if (process.env.NEXT_PUBLIC_TEST_VARIABLE !== "TEST_VALUE") {
+    throw new Error("Env variable not present");
+  }
+
+  res
+    .status(200)
+    .json({ deploymentIdentifier: "DEPLOYMENT_IDENTIFIER" })
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export const Home = () => {
+  const envVariable = process.env.NEXT_PUBLIC_TEST_VARIABLE;
+
   return (
     <div >
-      <p>Home</p>
+      {envVariable === "TEST_VALUE" && <p>DEPLOYMENT_IDENTIFIER</p>}
     </div>
   )
 }
