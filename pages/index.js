f6a70c294cda1f9c71b616115c0142e459438d1b
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-09 16:08:25 +0530

    nextjs-commit-try-27

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export const Home = () => {
 
   return (
     <div >
-      {envVariable === "TEST_VALUE" && <p>nextjs-commit-try-25</p>}
+      {envVariable === "TEST_VALUE" && <p>nextjs-commit-try-27</p>}
     </div>
   )
 }
