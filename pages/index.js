d96c0c0a0282008de13cd99e76aa04019c8dc8cb
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-09 16:58:51 +0530

    nextjs-commit-working-1

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export const Home = () => {
 
   return (
     <div >
-      {envVariable === "TEST_VALUE" && <p>nextjs-commit-working</p>}
+      {envVariable === "TEST_VALUE" && <p>nextjs-commit-working-1</p>}
     </div>
   )
 }
