3584e1d295155b02cc553a75a6169be92e20d60c
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-10 17:01:37 +0530

    some-random-id-udpated

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export const Home = () => {
 
   return (
     <div >
-      {envVariable === "TEST_VALUE" && <p>some-random-id-udpated-o</p>}
+      {envVariable === "TEST_VALUE" && <p>some-random-id-udpated</p>}
     </div>
   )
 }
