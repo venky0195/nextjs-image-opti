a4be219d361663353eb7a40055512e514a75337b
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-10 18:18:22 +0530

    some-random-id:6:18

--- pages/api/cloud-function.js
+++ pages/api/cloud-function.js
@@ -5,5 +5,5 @@ export default function handler(_, res) {
 
   res
     .status(200)
-    .json({ deploymentIdentifier: "some-random-id:6:14" })
+    .json({ deploymentIdentifier: "some-random-id:6:18" })
 }
