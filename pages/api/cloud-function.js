19d7009f899bedd08ab5f6d30775bfe2c2da5370
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-10 12:00:46 +0530

    nextjs-commit-working-3331

--- pages/api/cloud-function.js
+++ pages/api/cloud-function.js
@@ -5,5 +5,5 @@ export default function handler(_, res) {
 
   res
     .status(200)
-    .json({ deploymentIdentifier: "nextjs-commit-working-313" })
+    .json({ deploymentIdentifier: "nextjs-commit-working-3331" })
 }
