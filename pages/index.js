f547ed4e35799be809f69e5336f2fc0f476c818c
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-10 18:03:57 +0530

    some-random-id-updated-deployment-last-u

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export const Home = () => {
 
   return (
     <div >
-      {envVariable === "TEST_VALUE" && <p>some-random-id-updated-deployment-last</p>}
+      {envVariable === "TEST_VALUE" && <p>some-random-id-updated-deployment-last-u</p>}
     </div>
   )
 }
