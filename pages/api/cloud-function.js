18223c4db021ddb3b8e7048dba73bcbf60fc7d30
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-11 12:23:25 +0530

    contentfly-management-service:ab123:run-counter-5

--- pages/api/cloud-function.js
+++ pages/api/cloud-function.js
@@ -5,5 +5,5 @@ export default function handler(_, res) {
 
   res
     .status(200)
-    .json({ deploymentIdentifier: "contentfly-management-service:ab123:run-counter-3" })
+    .json({ deploymentIdentifier: "contentfly-management-service:ab123:run-counter-5" })
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export const Home = () => {
 
   return (
     <div >
-      {envVariable === "TEST_VALUE" && <p>contentfly-management-service:ab123:run-counter-3</p>}
+      {envVariable === "TEST_VALUE" && <p>contentfly-management-service:ab123:run-counter-5</p>}
     </div>
   )
 }
