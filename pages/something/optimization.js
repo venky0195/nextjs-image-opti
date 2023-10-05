0b94eeded876e98b09bc4594d49264ba9d1463a1
anujcontentstack <anuj.pundalik@contentstack.com>
2023-10-05 23:14:05 +0530

    Use image loader

--- pages/something/optimization.js
+++ pages/something/optimization.js
@@ -1,10 +1,19 @@
+'use client'
+
 import Image from 'next/image';
+
+const imageLoader = ({ src, width, quality }) => {
+    return `https://images.contentstack.io/v3/assets/bltb564490bc5201f31/blte3f202a35a967ea1/651c2d19a452a245cc8b4c35${src}?w=${width}&q=${quality || 75}`
+}
 
 export default function Page() {
     return (
         <Image
-            src={"https://images.contentstack.io/v3/assets/bltb564490bc5201f31/blte3f202a35a967ea1/651c2d19a452a245cc8b4c35/auto-generated-answers-ai.gif"}
+            loader={imageLoader}
+            src="auto-generated-answers-ai.gif"
             alt="Picture of the author"
+            width={500}
+            height={500}
         />
     )
 }
