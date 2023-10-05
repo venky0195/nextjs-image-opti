8b35336c16c052ca23bd1ccaf8bb540298dd49bf
anujcontentstack <anuj.pundalik@contentstack.com>
2023-10-05 16:01:15 +0530

    Use url in image optimization

--- pages/something/optimization.js
+++ pages/something/optimization.js
@@ -1,11 +1,9 @@
 import Image from 'next/image';
 
-import profilePic from './some-image.png';
-
 export default function Page() {
     return (
         <Image
-            src={profilePic}
+            src={"https://images.contentstack.io/v3/assets/bltb6904bc276687b34/blte3cfda09e7904b92/651e8718c997c6f3731f3539/651e867d1aec3e731914790b-preview.png"}
             alt="Picture of the author"
         />
     )
