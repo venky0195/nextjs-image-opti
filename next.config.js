c937bff72b46ebb17d93e4fda7bd918ac1e2033c
anujcontentstack <anuj.pundalik@contentstack.com>
2023-10-05 17:22:56 +0530

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.contentstack.io',
+                port: '',
+                pathname: '/v3/assets/bltb564490bc5201f31/blte3f202a35a967ea1/651c2d19a452a245cc8b4c35/auto-generated-answers-ai.gif',
+            },
+        ],
+    },
+}
