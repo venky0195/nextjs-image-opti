b43b78f2a6c0b5cd548fd7b271f8313b32a01fbf
anujcontentstack <anuj.pundalik@contentstack.com>
2023-10-06 17:59:13 +0530

    Update uri

--- pages/test.js
+++ pages/test.js
@@ -10,9 +10,10 @@ export default function Page() {
     return (
         <Image
             alt="The guitarist in the concert."
-            src="https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80"
-            width={2250}
-            height={1390}
+            src={`https://images.contentstack.io/v3/assets/bltb564490bc5201f31/blte3f202a35a967ea1/651c2d19a452a245cc8b4c35/auto-generated-answers-ai.gif`}
+            width={1200}
+            height={400}
+            quality={100}
             layout="responsive"
         />
     )
