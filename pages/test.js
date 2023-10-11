663897285db2a987aac70721f415d221d10dc5fe
anujcontentstack <anuj.pundalik@contentstack.com>
2023-10-11 16:33:46 +0530

    Use contentstack image

--- pages/test.js
+++ pages/test.js
@@ -2,16 +2,16 @@
 
 import Image from 'next/image';
 
-const imageLoader = ({ src, width, quality }) => {
-    return `https://images.contentstack.io/v3/assets/bltb564490bc5201f31/blte3f202a35a967ea1/651c2d19a452a245cc8b4c35${src}?w=${width}&q=${quality || 75}`
-}
+// const imageLoader = ({ src, width, quality }) => {
+//     return `https://images.contentstack.io/v3/assets/bltb564490bc5201f31/blte3f202a35a967ea1/651c2d19a452a245cc8b4c35${src}?w=${width}&q=${quality || 75}`
+// }
 
 export default function Page() {
 
     return (
         <Image
             alt="The guitarist in the concert."
-            src={`https://media.giphy.com/media/l2be3XyhClViY06dTt/giphy.gif`}
+            src={`https://images.contentstack.io/v3/assets/bltb564490bc5201f31/blte3f202a35a967ea1/651c2d19a452a245cc8b4c35/auto-generated-answers-ai.gif`}
             width={1200}
             height={400}
             quality={100}
