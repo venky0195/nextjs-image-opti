95fe216ecd11631b2b0c5c0f77b87c558009ec7c
anujcontentstack <anuj.pundalik@contentstack.com>
2023-12-22 11:20:26 +0530

    check webp support

--- pages/test.js
+++ pages/test.js
@@ -11,9 +11,9 @@ export default function Page() {
     return (
         <Image
             alt="The guitarist in the concert."
-            src={`https://images.contentstack.io/v3/assets/bltb564490bc5201f31/blte3f202a35a967ea1/651c2d19a452a245cc8b4c35/auto-generated-answers-ai.gif`}
-            width={1200}
-            height={400}
+            src={`https://images.contentstack.io/v3/assets/blteae40eb499811073/bltc5064f36b5855343/59e0c41ac0eddd140d5a8e3e/owl.jpg?auto=webp`}
+            width={1500}
+            height={1500}
             quality={100}
             layout="responsive"
         />
